c318e849d7391c3680bfa936190372ab6b14b91f
Facundo Maidana <facundo.maidana@studio.unibo.it>
2023-10-10 11:21:01 +0200

    inpainting reward model

--- rewards.py
+++ rewards.py
@@ -3,6 +3,33 @@ import io
 import numpy as np
 import torch
 
+def inpainting_score(inpainted_image, mask, original_image):
+    # Ensure images, original_image, and masks are in the right format
+    if isinstance(inpainted_image, torch.Tensor):
+        inpainted_image = inpainted_image.cpu().numpy()
+
+    if isinstance(original_image, torch.Tensor):
+        original_image = original_image.cpu().numpy()
+
+    if isinstance(mask, torch.Tensor):
+        mask = mask.cpu().numpy()
+
+
+    # Initialize a variable to store the cumulative L1 distance
+    total_distance = 0.0
+
+    for i, m, o in zip(inpainted_image, mask, original_image):
+        # Calculate L1 distance for pixels where the mask is 0
+        distance = np.abs(o - i) * (1 - m)
+
+        # Sum the L1 distances for this image
+        total_distance += distance.mean()
+
+    # Calculate the score (negative L1 distance) for reinforcement learning
+    score = -total_distance
+
+    return np.array([score])
+
 
 def jpeg_incompressibility(images, masks):
     if isinstance(images, torch.Tensor):
@@ -26,7 +53,7 @@ def jpeg_compressibility(images, masks):
     return -rew
 
 
-def aesthetic_score(scorer, images):
+def aesthetic_score(scorer, images, masks):
     images = (images * 255).round().clamp(0, 255).to(torch.uint8)
     scores = scorer(images)
     return scores
